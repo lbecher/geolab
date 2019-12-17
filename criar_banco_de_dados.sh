fa8147ff456abe95586edd80f310addf9d3959a8
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2019-12-17 14:02:16 -0300

    Update criar_banco_de_dados.sh

--- criar_banco_de_dados.sh
+++ criar_banco_de_dados.sh
@@ -1,1 +1,25 @@
 
+function criar_bd {
+	echo "";
+	read -p "Digite um nome para o novo banco de dados: " nome;
+	echo "":
+	psql -c "CREATE DATABASE \"$nome\" WITH ENCODING = 'UTF8' CONNECTION LIMIT = -1;";
+	psql -c "CREATE GROUP \"${nome}_nivel-0\";";
+	psql -c "CREATE GROUP \"${nome}_nivel-1\";";
+	psql -c "CREATE GROUP \"${nome}_nivel-2\";";
+	psql -c "GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA public TO GROUP \"${nome}_nivel-0\";";
+	psql -c "GRANT SELECT, INSERT, UPDATE, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA public TO GROUP \"${nome}_nivel-1\";";
+	psql -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO GROUP \"${nome}_nivel-2\";";
+	echo "";
+}
+
+clear;
+echo "Script para criar banco de dados.";
+echo "Isso vai modificar seu servidor!";
+read -p "Continuar? [S/n] " escolha;
+if [ "$escolha" == "s" ] || [ "$escolha" == "S" ];
+then
+	criar_bd;
+else
+	echo "Script abortado!";
+fi
