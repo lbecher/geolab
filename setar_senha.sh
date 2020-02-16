4843eeddd2017e095ea10392a65b2cf14b279020
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2020-02-16 12:09:19 -0300

    Update setar_senha.sh

--- setar_senha.sh
+++ setar_senha.sh
@@ -1,18 +1,19 @@
 function continuar {
-for i in 1 2 3;
-do
-read -p "Nova senha: " senha;
-read -p "Confirme a nova senha: " senha_c;
-if [ "$senha" == "$senha_c" ]
-then
-usermod -p $(openssl passwd -1 $senha) postgres;
-su -c "psql -c \"ALTER USER postgres WITH PASSWORD '${senha}'\";" -s /bin/sh postgres;
-i=3;
-else
-echo "As senhas não combinam!";
-fi
-done
+	for i in 1 2 3;
+	do
+		read -p "Nova senha: " senha;
+		read -p "Confirme a nova senha: " senha_c;
+		if [ "$senha" == "$senha_c" ]
+		then
+			sudo usermod -p $(openssl passwd -1 $senha) postgres;
+			sudo su -c "psql -c \"ALTER USER postgres WITH PASSWORD '${senha}'\";" -s /bin/sh postgres;
+			i=3;
+		else
+			echo "As senhas não combinam!";
+		fi
+	done
 }
+
 clear;
 echo "Script para setar senha do banco de dados.";
 echo "Isso vai modificar seu servidor!";
