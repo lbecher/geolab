608cbd4cbf9bc5532d529cbca3c4e9b964e25f0a
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2020-02-17 11:53:39 -0300

    Create instalar_ambiente_grafico.sh

---
+++ instalar_ambiente_grafico.sh
@@ -0,0 +1,3 @@
+sudo apt update;
+sudo apt upgrade;
+sudo apt install mesa-vulkan-drivers xwayland gnome-session-wayland gnome-terminal gnome-software nautilus chromium-browser chromium-browser-l10n;
