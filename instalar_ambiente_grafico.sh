10728a2fb1329c83b3abd4ca6de2fd12a7962258
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2020-04-16 14:53:24 -0300

    Update instalar_ambiente_grafico.sh

--- instalar_ambiente_grafico.sh
+++ instalar_ambiente_grafico.sh
@@ -1,3 +1,3 @@
 sudo apt update;
 sudo apt upgrade;
-sudo apt install mesa-vulkan-drivers xorg gnome-session gnome-terminal gnome-system-monitor gedit gnome-software nautilus chromium-browser chromium-browser-l10n;
+sudo apt install mesa-vulkan-drivers xorg gnome-session gnome-terminal gnome-system-monitor gedit gnome-software nautilus firefox firefox-locale-br;
