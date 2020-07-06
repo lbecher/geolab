78e33cd58948d8a614a51dd70abfc47868593395
Luiz Fernando Becher de Araujo <47784574+lbecher@users.noreply.github.com>
2020-07-06 12:06:21 -0300

    Update instalar_ambiente_grafico.sh

--- instalar_ambiente_grafico.sh
+++ instalar_ambiente_grafico.sh
@@ -1,3 +1,3 @@
 sudo apt update;
 sudo apt upgrade;
-sudo apt install xorg gnome-session gnome-software gnome-terminal nautilus kate firefox firefox-locale-br;
+sudo apt install xorg gnome-session gnome-software gnome-terminal nautilus gedit firefox firefox-locale-br;
